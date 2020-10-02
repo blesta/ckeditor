abffae9cae1087adf0a81afd5187aad4ea90ab7f
Abdy Franco <yosoy@abdyfran.co>
2020-10-02 14:14:37 -0600

    Update CKEditor to v5

---
+++ build/translations/oc.js
@@ -0,0 +1,1 @@
+(function(r){const o=r["oc"]=r["oc"]||{};o.dictionary=Object.assign(o.dictionary||{},{Bold:"Gras",Cancel:"Anullar",Code:"",Italic:"Italica","Remove color":"",Save:"Enregistrar",Strikethrough:"",Subscript:"",Superscript:""});o.getPluralForm=function(r){return r>1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
